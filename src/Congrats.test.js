256197d5b540db05833756a24a3a334bc2ab77cd
abhishek.jain <abhishek.jain@avalara.com>
2020-09-10 15:31:43 +0530

    Add proptypes

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -2,14 +2,18 @@ import React from "react";
 import Enzyme, { shallow, ShallowWrapper } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
+import checkPropTypes from "check-prop-types";
 
-import { findByTestAttr } from "../test/testUtils";
+import { findByTestAttr, checkProps } from "../test/testUtils";
 import Congrats from "./Congrats";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const defaultProps = { success: false };
+
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />);
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
 };
 
 /**
@@ -35,4 +39,9 @@ test("renders non-empty congrats message when success prop is true", () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "component-congrats");
   expect(message.text().length).not.toBe(0);
+});
+
+test("does not throw warning with expected props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
